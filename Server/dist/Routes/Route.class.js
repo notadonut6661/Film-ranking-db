f4309dd4701829bd4fc81996f06d625f82fe2b49
notadonut6661 <wotfan1989@gmail.com>
2023-02-19 20:06:13 +0200

    Added protected method getDecodedURI to Route.class.ts

--- Server/dist/Routes/Route.class.js
+++ Server/dist/Routes/Route.class.js
@@ -1,5 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const uriDecoder_1 = require("../helpers/uriDecoder");
+const uriParamsTypes_1 = require("../data/uriParamsTypes");
 class Route {
+    getDecodedURI(HTTPMethod, uri) {
+        const uriDecoderE = new uriDecoder_1.uriDecoder(uriParamsTypes_1.uriParamsType[this.routeName][HTTPMethod]);
+        return uriDecoderE.Decode(uri);
+    }
 }
 exports.default = Route;
