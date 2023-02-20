34f31879b162e9f4ec0c44c5fbbfd4f71672cf91
notadonut6661 <wotfan1989@gmail.com>
2023-02-20 18:20:45 +0200

    Update Film.class.js

--- Server/dist/Routes/Film.class.js
+++ Server/dist/Routes/Film.class.js
@@ -1,20 +1,32 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Film = void 0;
+const dbConnection_1 = __importDefault(require("../helpers/dbConnection"));
 const Route_class_1 = __importDefault(require("./Route.class"));
-const requestTypes_interface_1 = require("../data/requestTypes.interface");
 class Film extends Route_class_1.default {
     constructor() {
         super();
         this.Get = this.Get.bind(this);
+        this.routeName = "Film";
     }
     Get(req, res) {
-        console.log(this);
-        const { id } = this.getDecodedURI(requestTypes_interface_1.RequestType.GET, req.baseUrl);
-        // res.json(await (await dbConnection).query(`SELECT * FROM films WHERE id = ${id}`));
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log(this);
+            const { id } = this.getDecodedURI("GET", req.baseUrl);
+            res.json(yield (yield dbConnection_1.default).query(`SELECT * FROM films WHERE id = ${id}`));
+        });
     }
     // TODO: if  request content type is not equal to application/json we should response with an error
     Post(req, res) {
@@ -25,8 +37,6 @@ class Film extends Route_class_1.default {
     }
     Patch(req, res) {
         console.log(2);
-    }
-    getDecodedURI() {
     }
 }
 exports.Film = Film;
