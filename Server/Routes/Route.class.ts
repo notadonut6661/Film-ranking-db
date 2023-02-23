cb2db25d5d013d6a33724757a02226a93f45eea1
notadonut6661 <wotfan1989@gmail.com>
2023-02-23 19:05:06 +0200

    Added authorization to base route class

--- Server/Routes/Route.class.ts
+++ Server/Routes/Route.class.ts
@@ -4,11 +4,13 @@ import { uriDecoder } from '../helpers/uriDecoder';
 import { uriParamsType } from "../data/interfaces/uriParamsTypes";
 import { ParamsDictionary } from "express-serve-static-core"
 import { ParsedQs } from "qs"
+import dbConnection from '../helpers/dbConnection';
+import { generateSha256 } from '../utils/generateSha256';
 
 export default abstract class Route {
 
-  protected abstract routeName:string;
-  protected abstract dbName:string;
+  protected abstract routeName: string;
+  protected abstract dbName: string;
   protected readonly postRequestDataType: string | Array<string>;
 
   constructor() {
@@ -16,9 +18,27 @@ export default abstract class Route {
     this.Get = this.Get.bind(this);
     this.Post = this.Post.bind(this);
   }
-  
-  protected Authorization(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>): string | undefined {
-    return req.headers.authorization?.split(' ')[1];
+
+  protected async Authorization(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>): Promise<boolean> {
+    const authHeader = req.headers.authorization?.split(' ')[1];
+    // FIXME validate with regex
+
+    const input = {
+      name: authHeader?.split(':')[0],
+      password: authHeader?.split(':')[1]
+    }
+
+    let realPass: string | undefined;
+
+    try {
+      realPass = await (await dbConnection).query(`SELECT * FROM users WHERE name = ${input.name}`);
+    } catch {
+      return false;
+    }
+
+    if(!realPass || !input.password) return false;
+
+    return realPass === generateSha256(input.password);
   }
 
   public abstract Get(req: Request, res: Response): void;
@@ -27,7 +47,7 @@ export default abstract class Route {
   public abstract Delete(req: Request, res: Response): void;
 
   protected getDecodedURI(HTTPMethod: RequestType, uri: string) {
-    
+
     const uriDecoderE = new uriDecoder(uriParamsType[this.routeName][HTTPMethod]);
 
     return uriDecoderE.Decode(uri);
--- Server/dist/Routes/Route.class.js
+++ Server/dist/Routes/Route.class.js
@@ -1,7 +1,20 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const uriDecoder_1 = require("../helpers/uriDecoder");
 const uriParamsTypes_1 = require("../data/interfaces/uriParamsTypes");
+const dbConnection_1 = __importDefault(require("../helpers/dbConnection"));
 class Route {
     constructor() {
         this.postRequestDataType = 'application/json';
@@ -10,7 +23,22 @@ class Route {
     }
     Authorization(req) {
         var _a;
-        return (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
+        return __awaiter(this, void 0, void 0, function* () {
+            const authHeader = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
+            // FIXME validate with regex
+            const input = {
+                name: authHeader === null || authHeader === void 0 ? void 0 : authHeader.split(':')[0],
+                password: authHeader === null || authHeader === void 0 ? void 0 : authHeader.split(':')[1]
+            };
+            let realPass;
+            try {
+                realPass = yield (yield dbConnection_1.default).query(`SELECT * FROM users WHERE name = ${input.name}`);
+            }
+            catch (_b) {
+                return false;
+            }
+            return realPass === input.password;
+        });
     }
     getDecodedURI(HTTPMethod, uri) {
         const uriDecoderE = new uriDecoder_1.uriDecoder(uriParamsTypes_1.uriParamsType[this.routeName][HTTPMethod]);
