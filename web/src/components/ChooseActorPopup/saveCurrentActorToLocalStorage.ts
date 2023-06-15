c2df5f863c05c3573bddc110b8cde5b589ebd796
notadonut6661 <wotfan1989@gmail.com>
2023-06-15 20:03:14 +0300

    Update saveCurrentActorToLocalStorage.ts
    If castElement is only 1 param, then it will only update 1 element i localstorage

--- web/src/components/ChooseActorPopup/saveCurrentActorToLocalStorage.ts
+++ web/src/components/ChooseActorPopup/saveCurrentActorToLocalStorage.ts
@@ -3,6 +3,11 @@ import { PopupStages } from "../../data/PopupStages.enum";
 import getActorLocalStorageName from "./getActorLocalStorageName";
 
 export default function saveCurrentActorToLocalStorage (currentPopupStage: PopupStages, id:number, castElement: CastElement)  {  
-  // if (currentPopupStage < PopupStages.Submitted) return;
-  window.localStorage.setItem(getActorLocalStorageName(id), JSON.stringify(castElement))
+  const LocalStoragePropValue = JSON.parse(window.localStorage.getItem(getActorLocalStorageName(id)) || '');
+
+  Object.entries(castElement).forEach(([key, value]) => {
+    LocalStoragePropValue[key] = value;
+  });
+
+  window.localStorage.setItem(getActorLocalStorageName(id), JSON.stringify(LocalStoragePropValue))
 }
