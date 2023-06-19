a0e39c26eecdf295fb0dc829264a8e9675d3dbdc
notadonut6661 <wotfan1989@gmail.com>
2023-06-20 00:58:13 +0300

    nvm

--- web/src/Pages/AddTitle/postTitle.ts
+++ web/src/Pages/AddTitle/postTitle.ts
@@ -4,25 +4,29 @@ import getActorsFromLocalStorage from "./getActorsFromLocalStorage";
 
 export default function postTitle(): void {
   function getPostData() {
-    return {
-      title: window.localStorage.getItem(getLocalStorageName('Title')),
-      category: window.localStorage.getItem(getLocalStorageName('Category')),
-      plot: window.localStorage.getItem(getLocalStorageName('Plot')),
-      services: window.localStorage.getItem('WatchOn'),
-      cast: getActorsFromLocalStorage()
-    }
-  }
+    const { category } = getPostData();
 
-  console.log(getActorsFromLocalStorage());
-  
-  fetch(`${config.server_url}/title/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(getPostData())
-  }).then(res => res.json()).then( val => {
-    console.log(val);
-    
-  }).catch(err => console.warn(err));
-}
+    function getPostData() {
+      return {
+        title: window.localStorage.getItem(getLocalStorageName('Title')),
+        category: window.localStorage.getItem(getLocalStorageName('Category')),
+        plot: window.localStorage.getItem(getLocalStorageName('Plot')),
+        services: window.localStorage.getItem('WatchOn'),
+        cast: getActorsFromLocalStorage()
+      }
+    }
+
+    console.log(getActorsFromLocalStorage());
+
+    fetch(`${config.server_url}/${category?.toLocaleLowerCase()}/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(getPostData())
+    }).then(res => res.json()).then(val => {
+      console.log(val);
+
+    }).catch(err => console.warn(err));
+}}
+
