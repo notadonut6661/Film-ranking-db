f3a8b09b0900a505277bd7d0e14a0fb3702dfabf
notadonut6661 <wotfan1989@gmail.com>
2023-06-23 03:13:47 +0300

    SC

--- web/src/Pages/Signup/postNewUser.ts
+++ web/src/Pages/Signup/postNewUser.ts
@@ -1,3 +1,6 @@
+import { StatusCodes } from 'http-status-codes';
+import config from '../../data/Json/config.json';
+
 interface UserData {
   nickname: string;
   email: string;
@@ -6,5 +9,24 @@ interface UserData {
 }
 
 export default async function postNewUser(user: UserData): Promise<string | object> {
+  if(user.password !== user.confirmedPassword) {
+    return "Passwords do not match";
+  }
+
+  const signupResponse = await fetch(`${config.server_url}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+    })});
+    
+    if(signupResponse.status === StatusCodes.NOT_ACCEPTABLE) {
+      throw new Error("");
+    }
+
   return "";
 }
