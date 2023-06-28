32c27573bef0a539cf846b0f339d25ce18cb9bc7
notadonut6661 <wotfan1989@gmail.com>
2023-06-28 03:07:47 +0300

    Still working on image cropping

--- web/src/components/CropPopup/PosterCropPopup.tsx
+++ web/src/components/CropPopup/PosterCropPopup.tsx
@@ -1,32 +1,54 @@
 import "./style.scss";
-import { useCallback, useState, useMemo } from "react";
+import { useCallback, useState, useMemo, useRef, useEffect } from "react";
 
 export default function PosterCropPopup(): JSX.Element {
-  const isPosterRatioNormal = useState(true);
-  
+  const [isPosterRatioNormal, setIsPosterRatioNormal] = useState(true);
+  const cropArea = useRef<HTMLDivElement  | null>(null);
+  const imageCanvas = useRef<HTMLCanvasElement |null>(null);
+  const [cropAreaPos, setCropAreaPos] = useState(window.innerWidth / 2);
   const submitClickHandler = useCallback(() => {}, []);
-  const cropAreaSelectorDragHandler = useCallback(() => {}, []);
+
+  const cropAreaSelectorDragHandler = useCallback((ev:  React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      ev.preventDefault();
+      const left = ev.clientX - Number(cropArea.current?.getBoundingClientRect().left);
+
+      function onMouseMove(event: MouseEvent) {
+        if (event.pageX - left)
+        setCropAreaPos(event.pageX - left);
+      }
+
+      document.addEventListener('mousemove', onMouseMove);
+
+      document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', onMouseMove);
+      });
+
+    }, []);
   const cropAreaResizeHandler = useCallback(() => {}, [])
   
+
   return (
     <div className="poster-crop-popup">
       <div className="crop-editor">
-        <canvas />
-        <div id="crop-chooser">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+        <div id="current-image">
+          <canvas ref={imageCanvas}/>
+        </div>
+        <div id="crop-chooser"  style={{left: `${cropAreaPos}px`, top: `${imageCanvas.current?.getBoundingClientRect().top}px`}} ref={cropArea}>
+          <div id="move-crop-chooser" onMouseDown={cropAreaSelectorDragHandler} draggable="true"></div>
         </div>
       </div>
       <div className="control-panel">
-        <input type="button" id="submit"/>
-        <label>
-          <input type="radio" name="poster-ratio" />
-        </label>
-        <label>
-          <input type="radio" name="poster-ratio" />
-        </label>
+        <input type="button" id="submit" value="Submit" onClick={submitClickHandler}/>
+        <ul className="select-ratio">
+          <label id="normal-width">
+            <span>2:3</span>
+            <input type="radio" name="poster-ratio" />
+          </label>
+          <label id="double-width">
+            <span>4:3</span>
+            <input type="radio" name="poster-ratio" />
+          </label>
+        </ul>
       </div>
     </div>
   );
